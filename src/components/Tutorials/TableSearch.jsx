7bc9905019c05b6ab1bc006f0ea7fa676d5d8cca
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-21 10:49:05 +0400

    add data records

--- src/components/Tutorials/TableSearch.jsx
+++ src/components/Tutorials/TableSearch.jsx
@@ -1,28 +1,54 @@
 import { Table } from "antd";
 import { useState } from "react";
 
-export default function TableSearchTab(){
-    const [dataSource, setDataSource] = useState([]);
+export default function TableSearchTab() {
+    const [dataSource, setDataSource] = useState([
+        {
+            name: "John",
+            age: 32,
+            address: "New York"
+        },
+        {
+            name: "Jim",
+            age: 33,
+            address: "Sydney"
+        },
+        {
+            name: "David",
+            age: 40,
+            address: "Japan"
+        },
+        {
+            name: "James",
+            age: 36,
+            address: "New York"
+        },
+        {
+            name: "Sam",
+            age: 40,
+            address: "Sydney"
+        },
+    ]);
     const columns = [
         {
-            title:'Name',
-            dataIndex:'name'
+            title: 'Name',
+            dataIndex: 'name'
         },
         {
-            title:'Age',
-            dataIndex:'age'
+            title: 'Age',
+            dataIndex: 'age'
         },
         {
-            title:'Address',
-            dataIndex:'address'
+            title: 'Address',
+            dataIndex: 'address'
         }
     ]
 
     return (
         <div>
             <Table
-            columns={columns}
-            dataSource={dataSource}
+                columns={columns}
+                dataSource={dataSource}
             >
 
             </Table>
