d6af9f00b8f72ab2b144168cc031c0a102213871
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-22 14:39:15 +0400

    fixed

--- src/components/Tutorials/Theme.jsx
+++ src/components/Tutorials/Theme.jsx
@@ -1,16 +1,37 @@
 import { Button, DatePicker, Input, Progress, Slider, Space, Spin } from "antd";
+import { useState } from "react";
 
 export default function ThemeTab() {
-    return (
-        <div>
-            <Space>
-                <Button type="primary">Button</Button>
-                <Input placeholder="Type here..."></Input>
-                <Progress percent={50} type="circle" />
-                <Spin spinning />
-                <DatePicker />
-                <Slider />
-            </Space>
-        </div>
-    )
+
+    const IconSlider = ({ min, max }) => {
+        const [value, setValue] = useState(0);
+
+        return (
+            <div style={{ padding: 20 }}>
+                <Space direction="vertical" align="center" size="large" style={{ width: "100%" }}>
+
+                    <Button type="primary">Button</Button>
+
+                    <Input placeholder="Type here..." />
+
+                    <Progress percent={50} type="circle" />
+
+                    <Spin spinning />
+
+                    <DatePicker style={{ width: "100%" }} />
+
+                    <Slider
+                        min={min}
+                        max={max}
+                        value={value}
+                        onChange={setValue}
+                        style={{ width: 200 }}
+                    />
+
+                </Space>
+            </div>
+        );
+    };
+
+    return <IconSlider min={0} max={100} />;
 }
