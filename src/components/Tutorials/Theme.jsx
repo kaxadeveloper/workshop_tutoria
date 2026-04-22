b2216303ae73fdf53bec5d451f12c3a7cb8c6bf8
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-22 15:01:05 +0400

    add linear-gradient

--- src/components/Tutorials/Theme.jsx
+++ src/components/Tutorials/Theme.jsx
@@ -6,29 +6,69 @@ export default function ThemeTab() {
     const IconSlider = ({ min, max }) => {
         const [value, setValue] = useState(0);
 
+        const gradient = `
+            linear-gradient(
+                to top,
+                #fcc5e4 0%,
+                #fda34b 15%,
+                #ff7882 35%,
+                #c8699e 52%,
+                #7046aa 71%,
+                #0c1db8 87%,
+                #020f75 100%
+            )
+        `;
+
         return (
-            <div style={{ padding: 20 }}>
-                <Space direction="vertical" align="center" size="large" style={{ width: "100%" }}>
+            <div
+                style={{
+                    minHeight: "100vh",
+                    padding: 20,
+                    transition: "0.5s ease",
+                    position: "relative",
+                    background: "#fff",
+                    overflow: "hidden"
+                }}
+            >
+                {/* Background layer */}
+                <div
+                    style={{
+                        position: "absolute",
+                        inset: 0,
+                        backgroundImage: gradient,
+                        opacity: value / max,
+                        transition: "0.5s ease",
+                        zIndex: 0
+                    }}
+                />
 
-                    <Button type="primary">Button</Button>
+                {/* Content layer */}
+                <div style={{ position: "relative", zIndex: 1 }}>
+                    <Space
+                        direction="vertical"
+                        align="center"
+                        size="large"
+                        style={{ width: "100%" }}
+                    >
+                        <Button type="primary">Button</Button>
 
-                    <Input placeholder="Type here..." />
+                        <Input placeholder="Type here..." style={{ width: 200 }} />
 
-                    <Progress percent={50} type="circle" />
+                        <Progress percent={50} type="circle" />
 
-                    <Spin spinning />
+                        <Spin spinning />
 
-                    <DatePicker style={{ width: "100%" }} />
+                        <DatePicker style={{ width: 200 }} />
 
-                    <Slider
-                        min={min}
-                        max={max}
-                        value={value}
-                        onChange={setValue}
-                        style={{ width: 200 }}
-                    />
-
-                </Space>
+                        <Slider
+                            min={min}
+                            max={max}
+                            value={value}
+                            onChange={setValue}
+                            style={{ width: 200 }}
+                        />
+                    </Space>
+                </div>
             </div>
         );
     };
