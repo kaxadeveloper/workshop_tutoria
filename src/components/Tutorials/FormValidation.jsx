542984cfcff0fad1899aa5366537c48e94a41d52
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-23 09:11:20 +0400

    add email field

--- src/components/Tutorials/FormValidation.jsx
+++ src/components/Tutorials/FormValidation.jsx
@@ -1,11 +1,15 @@
-import { Form, Input } from "antd";
+import { Form, Button, Checkbox, DatePicker, Input, Select } from "antd";
 
 export default function FormValidationTab() {
     return (
         <div>
             <Form>
                 <Form.Item name="fullName" label="Full Name">
-                   <Input />
+                    <Input placeholder="Type your name" />
+                </Form.Item>
+
+                <Form.Item name="email" label="Email">
+                    <Input placeholder="Type your email" />
                 </Form.Item>
             </Form>
         </div>
