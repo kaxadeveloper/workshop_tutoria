4338de36b0d518897925b2ab1effe0850d3ace99
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-23 09:14:30 +0400

    add password field

--- src/components/Tutorials/FormValidation.jsx
+++ src/components/Tutorials/FormValidation.jsx
@@ -11,6 +11,10 @@ export default function FormValidationTab() {
                 <Form.Item name="email" label="Email">
                     <Input placeholder="Type your email" />
                 </Form.Item>
+
+                <Form.Item name="password" label="Password">
+                    <Input placeholder="Type your password" />
+                </Form.Item>
             </Form>
         </div>
     );
