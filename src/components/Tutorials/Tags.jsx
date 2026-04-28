ec27aeefa2c89e48f8d7a666b4a83e82f7364336
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-28 16:00:13 +0400

    add styles

--- src/components/Tutorials/Tags.jsx
+++ src/components/Tutorials/Tags.jsx
@@ -1,9 +1,17 @@
-import { Tag } from "antd";
+import { Space, Tag } from "antd";
 
 export default function TagsTab() {
     return (
-        <div>
-            <Tag>Tag</Tag>
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "100vh",
+        }}>
+            <Space direction="vertical" size={2}>
+                <Tag>Tag</Tag>
+                <Tag>Tag2</Tag>
+            </Space>
         </div>
     );
 }
