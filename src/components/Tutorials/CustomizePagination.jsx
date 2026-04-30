78c0eebe52174a3ff9a37d97277cd2a3e4624af1
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-30 12:23:39 +0400

    showSizeChanger prop

--- src/components/Tutorials/CustomizePagination.jsx
+++ src/components/Tutorials/CustomizePagination.jsx
@@ -3,9 +3,11 @@ import { Pagination, Space } from "antd";
 export default function CustomizePaginationTab() {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: "100vh" }}>
-            <Space size={8}>
+            <Space size={8} direction="vertical">
                 <Pagination total={20} pageSize={5} />
                 <Pagination defaultCurrent={2} total={20} pageSize={5} />
+                <Pagination defaultCurrent={2} total={200} pageSize={5} />
+                <Pagination total={20} showSizeChanger />
             </Space>
         </div>
     );
