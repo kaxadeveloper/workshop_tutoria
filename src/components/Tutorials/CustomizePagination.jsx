0c312fd0568ec376b0454b630c1d566e34fe3706
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-30 12:04:38 +0400

    add defaultCurrent prop

--- src/components/Tutorials/CustomizePagination.jsx
+++ src/components/Tutorials/CustomizePagination.jsx
@@ -4,6 +4,7 @@ export default function CustomizePaginationTab() {
     return (
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: "100vh" }}>
             <Pagination total={20} pageSize={5} />
+            <Pagination defaultCurrent={2} total={20} pageSize={5} />
         </div>
     );
 }
