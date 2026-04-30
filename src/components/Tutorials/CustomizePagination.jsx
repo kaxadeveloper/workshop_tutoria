e5d7eb5edcfa32c142c0c58dde0e8acce0848b87
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-30 12:12:20 +0400

    add Space component

--- src/components/Tutorials/CustomizePagination.jsx
+++ src/components/Tutorials/CustomizePagination.jsx
@@ -1,10 +1,12 @@
-import { Pagination } from "antd";
+import { Pagination, Space } from "antd";
 
 export default function CustomizePaginationTab() {
     return (
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: "100vh" }}>
-            <Pagination total={20} pageSize={5} />
-            <Pagination defaultCurrent={2} total={20} pageSize={5} />
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: "100vh" }}>
+            <Space size={8}>
+                <Pagination total={20} pageSize={5} />
+                <Pagination defaultCurrent={2} total={20} pageSize={5} />
+            </Space>
         </div>
     );
 }
