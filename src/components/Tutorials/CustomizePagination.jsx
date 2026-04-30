35f5f83ed22dba8788d25ec6a0f8e57e0206ca4d
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-04-30 12:00:57 +0400

    add total and pageSize

--- src/components/Tutorials/CustomizePagination.jsx
+++ src/components/Tutorials/CustomizePagination.jsx
@@ -2,10 +2,8 @@ import { Pagination } from "antd";
 
 export default function CustomizePaginationTab() {
     return (
-       <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: "100vh"}}>
-        <Pagination>
-            
-        </Pagination>
-       </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: "100vh" }}>
+            <Pagination total={20} pageSize={5} />
+        </div>
     );
 }
