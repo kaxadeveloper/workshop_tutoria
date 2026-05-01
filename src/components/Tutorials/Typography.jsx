181be6ae181385044091d6abd0666adbb532d53d
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-01 15:10:22 +0400

    add Title and Text subcomponents

--- src/components/Tutorials/Typography.jsx
+++ src/components/Tutorials/Typography.jsx
@@ -6,6 +6,12 @@ export default function TypographyTab() {
         <div>
            <Title level={3}>Code with CodingMaster</Title>
            <Title level={5}>Code with CodingMaster</Title>
+           <Text>Code with CodingMaster</Text>
+           <Text strong>Code with CodingMaster</Text>
+           <Text underline>Code with CodingMaster</Text>
+           <Text mark>Code with CodingMaster</Text>
+           <Text disabled>Code with CodingMaster</Text>
+           <Text type="success">Code with CodingMaster</Text>
         </div>
     );
 }
