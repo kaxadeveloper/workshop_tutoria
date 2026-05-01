4bffc8d3be6f262a17a0fa71213f1b16fc4350ab
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-01 15:15:56 +0400

    add space component

--- src/components/Tutorials/Typography.jsx
+++ src/components/Tutorials/Typography.jsx
@@ -1,17 +1,19 @@
-import { Typography } from "antd";
+import { Typography, Space } from "antd";
 const { Title, Text, Paragraph, Link } = Typography;
 
 export default function TypographyTab() {
     return (
         <div>
-           <Title level={3}>Code with CodingMaster</Title>
-           <Title level={5}>Code with CodingMaster</Title>
-           <Text>Code with CodingMaster</Text>
-           <Text strong>Code with CodingMaster</Text>
-           <Text underline>Code with CodingMaster</Text>
-           <Text mark>Code with CodingMaster</Text>
-           <Text disabled>Code with CodingMaster</Text>
-           <Text type="success">Code with CodingMaster</Text>
+            <Space direction="vertical">
+                <Title level={3}>Code with CodingMaster</Title>
+                <Title level={5}>Code with CodingMaster</Title>
+                <Text>Code with CodingMaster</Text>
+                <Text strong>Code with CodingMaster</Text>
+                <Text underline>Code with CodingMaster</Text>
+                <Text mark>Code with CodingMaster</Text>
+                <Text disabled>Code with CodingMaster</Text>
+                <Text type="success">Code with CodingMaster</Text>
+            </Space>
         </div>
     );
 }
