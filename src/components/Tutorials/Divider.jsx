e2656e86f9b8526db74206fced97be992855d85a
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-02 18:57:06 +0400

    add style to divider component

--- src/components/Tutorials/Divider.jsx
+++ src/components/Tutorials/Divider.jsx
@@ -14,7 +14,7 @@ export default function DividerTab() {
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, rem vitae adipisci laboriosam exercitationem, dignissimos aperiam quo nam consequuntur ea, hic quibusdam qui minus id!
             </p>
-            <Divider />
+            <Divider style={{ borderColor: "red" }}/>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident eaque nisi quae veritatis earum quis dignissimos, consectetur explicabo aliquid quasi ullam, recusandae a distinctio inventore!
             </p>
