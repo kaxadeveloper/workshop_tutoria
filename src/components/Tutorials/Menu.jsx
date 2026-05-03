1e67239670f39ff44c52c58c392a396b2a6db066
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-03 18:13:54 +0400

    add space component

--- src/components/Tutorials/Menu.jsx
+++ src/components/Tutorials/Menu.jsx
@@ -1,4 +1,4 @@
-import { Menu } from "antd";
+import { Menu, Space } from "antd";
 
 export default function MenuTab() {
     return (
@@ -9,21 +9,24 @@ export default function MenuTab() {
             justifyContent: "center",
             minHeight: "100vh"
         }}>
-            <Menu
-                items={[
-                    { label: "Home", key: "home" },
-                    {
-                        label: "Dashboard", 
-                        key: "dashboard", 
-                        children: [
-                            { label: "Revenue", key: "rev" },
-                            { label: "Expenses", key: "exp" },
-                        ],
-                    },
-                    { label: "User Management", key: "um" },
-                    { label: "Signout", key: "signout" },
-                ]}
-            ></Menu>
+            <Space>
+                <Menu
+                    mode="inline"
+                    items={[
+                        { label: "Home", key: "home" },
+                        {
+                            label: "Dashboard",
+                            key: "dashboard",
+                            children: [
+                                { label: "Revenue", key: "rev" },
+                                { label: "Expenses", key: "exp" },
+                            ],
+                        },
+                        { label: "User Management", key: "um" },
+                        { label: "Signout", key: "signout" },
+                    ]}
+                ></Menu>
+            </Space>
         </div>
     );
 }
