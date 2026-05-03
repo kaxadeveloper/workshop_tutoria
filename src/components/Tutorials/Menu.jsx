20a7f675cc6c8e72c8b18c47963d7ff23d77a0a6
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-03 18:05:24 +0400

    add children to Dashboard

--- src/components/Tutorials/Menu.jsx
+++ src/components/Tutorials/Menu.jsx
@@ -9,11 +9,21 @@ export default function MenuTab() {
             justifyContent: "center",
             minHeight: "100vh"
         }}>
-            <Menu items={[
-                { label: "Home", key: "home" },
-                { label: "Dashboard", key: "dashboard" },
-                { label: "User Management", key: "um" },
-            ]}></Menu>
+            <Menu
+                items={[
+                    { label: "Home", key: "home" },
+                    {
+                        label: "Dashboard", 
+                        key: "dashboard", 
+                        children: [
+                            { label: "Revenue", key: "rev" },
+                            { label: "Expenses", key: "exp" },
+                        ],
+                    },
+                    { label: "User Management", key: "um" },
+                    { label: "Signout", key: "signout" },
+                ]}
+            ></Menu>
         </div>
     );
 }
