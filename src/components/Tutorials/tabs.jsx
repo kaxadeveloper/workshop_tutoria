ab14e3f6d99aa2b036b09f082c32b326665f879b
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-04 16:04:33 +0400

    add space component

--- src/components/Tutorials/tabs.jsx
+++ src/components/Tutorials/tabs.jsx
@@ -1,4 +1,4 @@
-import { Tabs } from "antd";
+import { Space, Tabs } from "antd";
 
 export default function TabsTab() {
     return (
@@ -9,30 +9,31 @@ export default function TabsTab() {
             minHeight: "100vh",
         }}
         >
-            <Tabs defaultActiveKey="tab1">
-                <Tabs.TabPane tab="Tab 1" key="tab1">
-                    <div>This is a content of tab1</div>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Tab 2" key="tab2">
-                    <div>This is a content of tab2</div>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Tab 3" key="tab3">
-                    <div>This is a content of tab3</div>
-                </Tabs.TabPane>
-            </Tabs>
+            <Space size={20}>
+                <Tabs defaultActiveKey="tab1">
+                    <Tabs.TabPane tab="Tab 1" key="tab1">
+                        <div>This is a content of tab1</div>
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Tab 2" key="tab2">
+                        <div>This is a content of tab2</div>
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Tab 3" key="tab3">
+                        <div>This is a content of tab3</div>
+                    </Tabs.TabPane>
+                </Tabs>
 
-            <Tabs defaultActiveKey="tab1" tabPosition="left">
-                <Tabs.TabPane tab="Tab 1" key="tab1">
-                    <div>This is a content of tab1</div>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Tab 2" key="tab2">
-                    <div>This is a content of tab2</div>
-                </Tabs.TabPane>
-                <Tabs.TabPane tab="Tab 3" key="tab3">
-                    <div>This is a content of tab3</div>
-                </Tabs.TabPane>
-            </Tabs>
-
+                <Tabs defaultActiveKey="tab1" tabPosition="left">
+                    <Tabs.TabPane tab="Tab 1" key="tab1">
+                        <div>This is a content of tab1</div>
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Tab 2" key="tab2">
+                        <div>This is a content of tab2</div>
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Tab 3" key="tab3">
+                        <div>This is a content of tab3</div>
+                    </Tabs.TabPane>
+                </Tabs>
+            </Space>
         </div>
     );
 }
