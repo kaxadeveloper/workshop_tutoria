abaeb338da6ccbe4af8f19cb104d48e52eb1da7c
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-04 16:30:06 +0400

    add and delete options

--- src/components/Tutorials/tabs.jsx
+++ src/components/Tutorials/tabs.jsx
@@ -1,3 +1,4 @@
+import { AmazonOutlined, AndroidOutlined, AppleOutlined } from "@ant-design/icons";
 import { Space, Tabs } from "antd";
 
 export default function TabsTab() {
@@ -30,6 +31,33 @@ export default function TabsTab() {
                         <div>This is a content of tab2</div>
                     </Tabs.TabPane>
                     <Tabs.TabPane tab="Tab 3" key="tab3">
+                        <div>This is a content of tab3</div>
+                    </Tabs.TabPane>
+                </Tabs>
+
+                <Tabs defaultActiveKey="tab1">
+                    <Tabs.TabPane
+                        tab={
+                            <span>
+                                <AppleOutlined /> Apple  
+                            </span>
+                        }
+                        key="tab1"
+                    >
+                        <div>This is a content of tab1</div>
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab={
+                            <span>
+                                <AndroidOutlined /> Android  
+                            </span>
+                        } key="tab2">
+                        <div>This is a content of tab2</div>
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab={
+                            <span>
+                                <AmazonOutlined /> Amazon 
+                            </span>
+                        } key="tab3">
                         <div>This is a content of tab3</div>
                     </Tabs.TabPane>
                 </Tabs>
