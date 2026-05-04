107c00f1e6dd27f84d582c78178f30afa8cf3769
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-04 14:59:00 +0400

    add tab2, tab3

--- src/components/Tutorials/tabs.jsx
+++ src/components/Tutorials/tabs.jsx
@@ -13,6 +13,12 @@ export default function TabsTab() {
                 <Tabs.TabPane tab="Tab 1" key="tab1">
                     <div>This is a content of tab1</div>
                 </Tabs.TabPane>
+                <Tabs.TabPane tab="Tab 2" key="tab2">
+                    <div>This is a content of tab2</div>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Tab 3" key="tab3">
+                    <div>This is a content of tab3</div>
+                </Tabs.TabPane>
             </Tabs>
 
         </div>
