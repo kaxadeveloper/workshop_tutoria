2c522a531fbfce37fe2364b696e3d50e3600b8e5
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-05 13:04:14 +0400

    updated

--- src/components/Tutorials/Avatar.jsx
+++ src/components/Tutorials/Avatar.jsx
@@ -2,49 +2,84 @@ import { UserOutlined } from "@ant-design/icons";
 import { Avatar, Space } from "antd";
 
 export default function AvatarTab() {
+
+    // 🔹 Define all avatar types in one place
+    const avatars = [
+        {
+            key: "name",
+            content: "CodingMaster",
+            style: { backgroundColor: "green", color: "white" }
+        },
+        {
+            key: "letter",
+            content: "C",
+            shape: "square",
+            style: { backgroundColor: "black", color: "white" }
+        },
+        {
+            key: "logo",
+            src: "/logo.png"
+        },
+        {
+            key: "icon",
+            icon: <UserOutlined />
+        }
+    ];
+
     return (
         <div style={{
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
             minHeight: "100vh"
-        }}
-        >
-            <Space size={12} direction="vertical">
-                <Avatar style={{ backgroundColor: "green", color: "white" }}>
-                    CodingMaster
-                </Avatar>
-                <Avatar shape="square" style={{ backgroundColor: "black" }}>
-                    C
-                </Avatar>
-                <Avatar
-                    src="/logo.png"
-                />
-                <Avatar icon={<UserOutlined />} />
+        }}>
+            <Space size={20} direction="vertical">
+
+                {/* 🔹 Single Avatars */}
+                <Space>
+                    {avatars.map((item) => (
+                        <Avatar
+                            key={item.key}
+                            src={item.src}
+                            icon={item.icon}
+                            shape={item.shape || "circle"}
+                            style={item.style}
+                        >
+                            {item.content}
+                        </Avatar>
+                    ))}
+                </Space>
+
+                {/* 🔹 Avatar Group */}
                 <Avatar.Group>
-                    <Avatar style={{ backgroundColor: "green", color: "white" }}>
-                        CodingMaster
-                    </Avatar>
-                    <Avatar shape="circle" style={{ backgroundColor: "black" }}>
-                        C
-                    </Avatar>
-                    <Avatar
-                        src="/logo.png"
-                    />
-                    <Avatar icon={<UserOutlined />} />
+                    {avatars.map((item) => (
+                        <Avatar
+                            key={item.key}
+                            src={item.src}
+                            icon={item.icon}
+                            shape={item.shape || "circle"}
+                            style={item.style}
+                        >
+                            {item.content}
+                        </Avatar>
+                    ))}
                 </Avatar.Group>
+
+                {/* 🔹 Avatar Group with maxCount */}
                 <Avatar.Group maxCount={2}>
-                    <Avatar style={{ backgroundColor: "green", color: "white" }}>
-                        <span style={{ color: "red" }}> CodingMaster</span>
-                    </Avatar>
-                    <Avatar shape="circle" style={{ backgroundColor: "black" }}>
-                        C
-                    </Avatar>
-                    <Avatar
-                        src="/logo.png"
-                    />
-                    <Avatar icon={<UserOutlined />} />
+                    {avatars.map((item) => (
+                        <Avatar
+                            key={item.key}
+                            src={item.src}
+                            icon={item.icon}
+                            shape={item.shape || "circle"}
+                            style={item.style}
+                        >
+                            {item.content}
+                        </Avatar>
+                    ))}
                 </Avatar.Group>
+
             </Space>
         </div>
     );
