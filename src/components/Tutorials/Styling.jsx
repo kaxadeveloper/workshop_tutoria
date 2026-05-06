a0919b7189d532a6d2b7b9a8263319ff0b1281fe
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-06 16:05:10 +0400

    add table component

--- src/components/Tutorials/Styling.jsx
+++ src/components/Tutorials/Styling.jsx
@@ -1,4 +1,4 @@
-import { Button, DatePicker, Image, Input, Space, Typography } from "antd";
+import { Button, DatePicker, Image, Input, Space, Table, Typography } from "antd";
 import "./override.scss";
 
 function CustomInput1() {
@@ -47,8 +47,34 @@ export default function StylingTab() {
                 <DatePicker
                     style={{ width: "100%" }} className="datePicker" dropdownClassName="datePickerDropdown" />
 
-                    
+                <Table columns={[
+                    {
+                        title: "Col1",
+                        dataIndex: "col1"
+                    },
+                    {
+                        title: "Col2",
+                        dataIndex: "col2"
+                    },
+                    {
+                        title: "Col3",
+                        dataIndex: "col3"
+                    },
+                ]}
+                    dataSource={[
+                        {
+                            col1: "Row1-Col1",
+                            col2: "Row1-Col2",
+                            col3: "Row1-Col3",
+                        },
+                        {
+                            col1: "Row2-Col1",
+                            col2: "Row2-Col2",
+                            col3: "Row2-Col3",
+                        },
+                    ]}
+                ></Table>
             </Space>
-        </div>
+        </div >
     );
 }
