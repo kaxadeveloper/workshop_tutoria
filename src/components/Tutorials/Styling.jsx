45c793e873429e410a34fdf2f21440b3e2a818ff
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-06 12:46:03 +0400

    add override.scss file

--- src/components/Tutorials/Styling.jsx
+++ src/components/Tutorials/Styling.jsx
@@ -1,9 +1,11 @@
 import { Typography } from "antd";
+import "./override.scss";
 
 export default function StylingTab() {
     return (
         <div style={{
             display: "flex",
+            flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
             minHeight: "100vh"
@@ -14,8 +16,18 @@ export default function StylingTab() {
                     color: "red",
                     fontSize: 20,
                     fontWeight: "bold"
-                }}>
+                }}
+            >
                 CodingMaster
+            </Typography.Text>
+            <Typography.Text
+            style={{
+                color: "green",
+                fontSize: "20px",
+                fontWeight: "bold",
+            }}
+            >
+                Welcome Back!
             </Typography.Text>
         </div>
     );
