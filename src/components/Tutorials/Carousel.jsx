9cf76abbc51c22c0fcf272fd94659393cbfb44c5
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-07 11:54:05 +0400

    add Carousel component

--- src/components/Tutorials/Carousel.jsx
+++ src/components/Tutorials/Carousel.jsx
@@ -1,3 +1,4 @@
+import { Carousel } from "antd";
 
 export default function CarouselTab() {
     return (
@@ -7,9 +8,22 @@ export default function CarouselTab() {
             justifyContent: "center",
             minHeight: "100vh"
         }}>
-            <div style={{ width: "300px", height: "300px", backgroundColor: "orange" }}
+            <div style={{ width: "300px", height: "300px", backgroundColor: "transparent" }}
             >
-              
+                <Carousel>
+                    <div>
+                        <img style={{ height: "100%", width: "100%" }} src="/img1.jpg" />
+                    </div>
+                    <div>
+                        <img style={{ height: "100%", width: "100%" }} src="/img2.jpg" />
+                    </div>
+                    <div>
+                        <img style={{ height: "100%", width: "100%" }} src="/img3.jpg" />
+                    </div>
+                    <div>
+                        <img style={{ height: "100%", width: "100%" }} src="/img4.jpg" />
+                    </div>
+                </Carousel>
             </div>
         </div>
     );
