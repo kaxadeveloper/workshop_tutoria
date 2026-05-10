9f5139c7a597b279bc886637bc93f9588a1fafba
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-10 11:42:22 +0400

    add key to panel

--- src/components/Tutorials/accordion.jsx
+++ src/components/Tutorials/accordion.jsx
@@ -11,8 +11,14 @@ export default function AccordionTab() {
             }}
         >
             <Collapse>
-                <Collapse.Panel header="This is a panel 1">
+                <Collapse.Panel key={"1"} header="This is a panel 1">
                     <Typography.Text>This is a content of Panel 1</Typography.Text>
+                </Collapse.Panel>
+                <Collapse.Panel key={"2"} header="This is a panel 2">
+                    <Typography.Text>This is a content of Panel 2</Typography.Text>
+                </Collapse.Panel>
+                <Collapse.Panel key={"3"} header="This is a panel 3">
+                    <Typography.Text>This is a content of Panel 3</Typography.Text>
                 </Collapse.Panel>
             </Collapse>
         </div>
