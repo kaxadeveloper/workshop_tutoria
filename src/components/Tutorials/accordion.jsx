b7ff4e63416530b7324228404ed173588bdf455e
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-10 12:34:08 +0400

    add defaultActiveKey

--- src/components/Tutorials/accordion.jsx
+++ src/components/Tutorials/accordion.jsx
@@ -16,20 +16,24 @@ export default function AccordionTab() {
                 expandIcon={({ isActive }) => {
                     return <CaretRightFilled rotate={isActive ? 90 : 0} />
                 }}
-                expandIconPosition="end"
+                expandIconPosition="start"
+                defaultActiveKey={["1", "2"]}
             >
                 <Collapse.Panel key={"1"} header="This is a panel 1">
                     <Typography.Text>This is a content of Panel 1</Typography.Text>
                 </Collapse.Panel>
                 <Collapse.Panel
                     key={"2"}
+                    collapsible="disabled"
                     header={<div>This is a custom Panel Header!</div>}
                 >
                     <Typography.Text>This is a content of Panel 2</Typography.Text>
                 </Collapse.Panel>
                 <Collapse.Panel
-                    extra={<Button>Download</Button>} key={"3"}
-                    header="This is a panel 3">
+                    extra={<Button>Download</Button>}
+                    key={"3"}
+                    header="This is a panel 3"
+                >
                     <Typography.Text>This is a content of Panel 3</Typography.Text>
                 </Collapse.Panel>
                 <Collapse.Panel key={"4"} header="This is a panel 4">
