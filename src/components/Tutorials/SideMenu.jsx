e6295c8ca08cc766db27c6806c6b4f29db6f4cbe
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-14 11:43:31 +0400

    add BrowserRouter

--- src/components/Tutorials/SideMenu.jsx
+++ src/components/Tutorials/SideMenu.jsx
@@ -1,4 +1,5 @@
 import { Menu } from "antd";
+import { Routes, Route } from 'react-router-dom';
 
 export default function SideMenuTab() {
     return (
@@ -10,7 +11,9 @@ export default function SideMenuTab() {
         >
             <Menu
                 items={[
-                    { label: "Home" },
+                    { label: "Home",
+                        
+                     },
                     { label: "Dashboard" },
                     { label: "Users List" },
                     { label: "Profile" },
@@ -25,7 +28,12 @@ export default function SideMenuTab() {
 function Content() {
     return (
         <div>
-           
+            <Routes>
+                <Route path="/" element={<div>Home</div>}></Route>
+                <Route path="/dashboard" element={<div>Dashboard</div>}></Route>
+                <Route path="/usersList" element={<div>Users List</div>}></Route>
+                <Route path="/profile" element={<div>Profile</div>}></Route>
+            </Routes>
         </div>
     );
 }
