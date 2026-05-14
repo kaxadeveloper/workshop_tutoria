a85694458b7486483742147be04e29d51153e75d
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-14 12:48:57 +0400

    add Header and Footer components

--- src/components/Tutorials/SideMenu.jsx
+++ src/components/Tutorials/SideMenu.jsx
@@ -1,56 +1,76 @@
 import { DashboardOutlined, HomeOutlined, PoweroffOutlined, UnorderedListOutlined, UserOutlined } from "@ant-design/icons";
 import { Menu } from "antd";
+import { Layout } from 'antd';
+const { Header: AntHeader } = Layout;
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 export default function SideMenuTab() {
     const navigate = useNavigate();
     return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "row",
-            }}
-        >
-            <Menu
-                onClick={({ key }) => {
-                    if (key === "signout") {
-
-                    } else {
-                        navigate(key);
-                    }
-                }}
-                items={[
-                    {
-                        label: "Home",
-                        key: "/",
-                        icon: <HomeOutlined />
-                    },
-                    {
-                        label: "Dashboard",
-                        key: "/dashboard",
-                        icon: <DashboardOutlined />
-                    },
-                    {
-                        label: "Users List",
-                        key: "/usersList",
-                        icon: <UnorderedListOutlined />
-                    },
-                    {
-                        label: "Profile",
-                        key: "/profile",
-                        icon: <UserOutlined />
-                    },
-                    {
-                        label: "Signout",
-                        key: "signout",
-                        icon: <PoweroffOutlined />,
-                        danger: true
-                    },
-                ]}
-            ></Menu>
-            <Content />
+        <div>
+            <Header />
+            <div>
+                <SideMenu />
+                <Content />
+            </div>
+            <Footer />
         </div>
     );
+}
+
+function Header() {
+    return <div>Header</div>
+}
+
+function Footer() {
+    return <div>Footer</div>
+}
+
+function SideMenu() {
+    return (<div
+        style={{
+            display: "flex",
+            flexDirection: "row",
+        }}
+    >
+        <Menu
+            onClick={({ key }) => {
+                if (key === "signout") {
+
+                } else {
+                    navigate(key);
+                }
+            }}
+            items={[
+                {
+                    label: "Home",
+                    key: "/",
+                    icon: <HomeOutlined />
+                },
+                {
+                    label: "Dashboard",
+                    key: "/dashboard",
+                    icon: <DashboardOutlined />
+                },
+                {
+                    label: "Users List",
+                    key: "/usersList",
+                    icon: <UnorderedListOutlined />
+                },
+                {
+                    label: "Profile",
+                    key: "/profile",
+                    icon: <UserOutlined />
+                },
+                {
+                    label: "Signout",
+                    key: "signout",
+                    icon: <PoweroffOutlined />,
+                    danger: true
+                },
+            ]}
+        ></Menu>
+    </div>)
 }
 
 function Content() {
