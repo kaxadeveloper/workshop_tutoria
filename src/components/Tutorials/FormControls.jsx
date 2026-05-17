eb7379150e5bbfcdfdee2abcd54b10b1d4318aeb
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-17 14:12:21 +0400

    add PlayerScors function

--- src/components/Tutorials/FormControls.jsx
+++ src/components/Tutorials/FormControls.jsx
@@ -1,12 +1,26 @@
 import { Button, Form, Input } from "antd";
 
+function PlayerScors(props) {
+
+    return
+    <>
+     <Button>-</Button>
+     <span>{props.value}</span>
+     <Button>+</Button>
+    </>
+}
+
 export default function FormControlsTab() {
     return (
         <div>
             <Form
                 onFinish={(values) => {
 
-                }}>
+                }}
+                onFinishFailed={(failedValues) => {
+
+                }}
+            >
                 <Form.Item
                     name={"playerName"}
                     label="Player Name"
@@ -14,6 +28,14 @@ export default function FormControlsTab() {
                     rules={[{ required: true, message: "Please enter player name." }]}
                 >
                     <Input />
+                </Form.Item>
+                <Form.Item
+                    name={"playerName"}
+                    label="Player Name"
+                    required
+                    rules={[{ required: true, message: "Please enter player name." }]}
+                >
+                    <PlayerScors />
                 </Form.Item>
                 <Button
                     htmlType="submit"
