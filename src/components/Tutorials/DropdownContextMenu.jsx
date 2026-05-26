00d2f28fb7311e570bb3a9da93bf29741aaf821c
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-26 13:43:49 +0400

    add Dropdown component

--- src/components/Tutorials/DropdownContextMenu.jsx
+++ src/components/Tutorials/DropdownContextMenu.jsx
@@ -1,17 +1,20 @@
+import { Dropdown } from "antd";
 
 export default function DropdownContextMenuTab() {
     return (
         <div>
-            <div
-                style={{
-                    width: 200,
-                    height: 200,
-                    backgroundColor: "lightgray",
-                    border: "1px dashed red",
-                }}
-            >
-                Right Click Me
-            </div>
+            <Dropdown>
+                <div
+                    style={{
+                        width: 200,
+                        height: 200,
+                        backgroundColor: "lightgray",
+                        border: "1px dashed red",
+                    }}
+                >
+                    Right Click Me
+                </div>
+            </Dropdown>
         </div>
     )
 }
