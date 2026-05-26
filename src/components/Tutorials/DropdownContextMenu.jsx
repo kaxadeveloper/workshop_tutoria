864e48d03111a5c440249d618438d821f0f2824d
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-26 14:57:04 +0400

    add final version

--- src/components/Tutorials/DropdownContextMenu.jsx
+++ src/components/Tutorials/DropdownContextMenu.jsx
@@ -1,74 +1,114 @@
-import { CopyOutlined, DeleteOutlined, FacebookFilled, InstagramFilled, ReloadOutlined, ShareAltOutlined, TwitterCircleFilled } from "@ant-design/icons";
-import { Dropdown, Menu } from "antd";
-
-export default function DropdownContextMenuTab() {
-    const menu = <Menu
-        items={[
-            {
-                label: "Copy",
-                key: "copy",
-                icon: <CopyOutlined />,
-            },
-            {
-                label: "Share",
-                key: "share",
-                icon: <ShareAltOutlined />,
-                children: [
-                    {
-                        label: "Facebook",
-                        key: "fb",
-                        icon: <FacebookFilled />,
-                    },
-                    {
-                        label: "Twitter",
-                        key: "tw",
-                        icon: <TwitterCircleFilled />,
-                    },
-                    {
-                        label: "Instagram",
-                        key: "in",
-                        icon: <InstagramFilled />,
-                    },
-                ]
-            },
-            {
-                label: "Reload",
-                key: "reload",
-                icon: <ReloadOutlined />,
-            },
-            {
-                label: "Delete",
-                key: "delete",
-                danger: true,
-                icon: <DeleteOutlined />,
-            },
-        ]}>
-
-    </Menu>
+import {
+    CopyOutlined,
+    DeleteOutlined,
+    FacebookFilled,
+    InstagramFilled,
+    ReloadOutlined,
+    ShareAltOutlined,
+    TwitterCircleFilled,
+  } from "@ant-design/icons";
+  
+  import { Dropdown, message } from "antd";
+  
+  export default function DropdownContextMenuTab() {
+    const items = [
+      {
+        label: "Copy",
+        key: "copy",
+        icon: <CopyOutlined />,
+      },
+      {
+        label: "Share",
+        key: "share",
+        icon: <ShareAltOutlined />,
+        children: [
+          {
+            label: "Facebook",
+            key: "fb",
+            icon: <FacebookFilled />,
+          },
+          {
+            label: "Twitter",
+            key: "tw",
+            icon: <TwitterCircleFilled />,
+          },
+          {
+            label: "Instagram",
+            key: "in",
+            icon: <InstagramFilled />,
+          },
+        ],
+      },
+      {
+        label: "Reload",
+        key: "reload",
+        icon: <ReloadOutlined />,
+      },
+      {
+        label: "Delete",
+        key: "delete",
+        danger: true,
+        icon: <DeleteOutlined />,
+      },
+    ];
+  
+    const handleMenuClick = ({ key }) => {
+      message.info(`You clicked: ${key}`);
+    };
+  
     return (
-        <div
-            style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                minHeight: "100vh",
-            }}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "40px",
+          minHeight: "100vh",
+          flexDirection: "column",
+        }}
+      >
+        {/* Context Menu Dropdown */}
+        <Dropdown
+          menu={{
+            items,
+            onClick: handleMenuClick,
+          }}
+          trigger={["contextMenu"]}
         >
-            <Dropdown
-                overlay={menu}
-                trigger={["contextMenu"]}
-            >
-                <div
-                    style={{
-                        width: 200,
-                        height: 200,
-                        backgroundColor: "lightgray",
-                        border: "1px dashed red",
-                    }}
-                >
-                    Right Click Me
-                </div>
-            </Dropdown>
-        </div>
-    )
-}
+          <div
+            style={{
+              width: 200,
+              height: 200,
+              backgroundColor: "lightgray",
+              border: "1px dashed red",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "context-menu",
+              userSelect: "none",
+            }}
+          >
+            Right Click Me
+          </div>
+        </Dropdown>
+  
+        {/* Click Dropdown */}
+        <Dropdown
+          menu={{
+            items,
+            onClick: handleMenuClick,
+          }}
+          trigger={["click"]}
+        >
+          <p
+            style={{
+              cursor: "pointer",
+              fontSize: "18px",
+            }}
+          >
+            Click Me
+          </p>
+        </Dropdown>
+      </div>
+    );
+  }
